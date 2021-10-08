aed3859bcf4feea9426b478c44f784937afb8a0d
Michelle Goenawan <M.Goenawan@MICH-Air.fios-router.home>
2021-10-08 00:26:48 -0400

    commented out the testing commands

--- hw2/webdev_hw2.js
+++ hw2/webdev_hw2.js
@@ -13,18 +13,18 @@ Array.prototype.myEach = function(callbackFn) {
 };
 
 
-/* TEST EACH  
-const arr=[1,2,3,4,5,6,7,8];
+    /* TEST EACH  
+    const arr=[1,2,3,4,5,6,7,8];
 
-console.log("myEach: ")
-arr.myEach((x) => console.log(x) );
+    console.log("myEach: ")
+    arr.myEach((x) => console.log(x) );
 
-console.log("forEach: ")
-arr.forEach( (x) => console.log(x) );
+    console.log("forEach: ")
+    arr.forEach( (x) => console.log(x) );
 
-const isEven = (number) => console.log(number%2 === 0);
-arr.myEach( isEven ); 
-*/
+    const isEven = (number) => console.log(number%2 === 0);
+    arr.myEach( isEven ); 
+    */
 
 
 // MAP //
@@ -37,10 +37,10 @@ Array.prototype.myMap = function(callbackFn) {
     return thisnew;
 };
 
-/* TEST MAP
-console.log("Elements cubed:",arr.myMap(num => num * num * num));
-console.log("-----------\n");
-*/
+    /* TEST MAP
+    console.log("Elements cubed:",arr.myMap(num => num * num * num));
+    console.log("-----------\n");
+    */
 
 // FILTER //
 Array.prototype.myFilter = function(callbackFn) {
@@ -56,18 +56,18 @@ Array.prototype.myFilter = function(callbackFn) {
 };
 
 
-/* TEST FILTER 
-const words = ['spray', 'limit', 'elite', 'exuberant', 'destruction', 'present'];
+    /* TEST FILTER 
+    const words = ['spray', 'limit', 'elite', 'exuberant', 'destruction', 'present'];
 
-const result = words.filter(word => word.length > 6);
-const result2 = words.myFilter(word => word.length > 6);
+    const result = words.filter(word => word.length > 6);
+    const result2 = words.myFilter(word => word.length > 6);
 
-console.log(result);
-console.log(result2);
+    console.log(result);
+    console.log(result2);
 
 
-// expected output: Array ["exuberant", "destruction", "present"]
-*/
+    // expected output: Array ["exuberant", "destruction", "present"]
+    */
 
 // SOME //
 Array.prototype.mySome = function(callbackFn) {
@@ -81,17 +81,17 @@ Array.prototype.mySome = function(callbackFn) {
     return false;
 };
 
-/* TEST SOME 
-const array = [1, 2, 3, 4, 5];
+    /* TEST SOME 
+    const array = [1, 2, 3, 4, 5];
 
-// checks whether an element is even
-const even = (element) => element % 2 === 0;
-const even2 = (element) => element % 2 === 0;
+    // checks whether an element is even
+    const even = (element) => element % 2 === 0;
+    const even2 = (element) => element % 2 === 0;
 
-console.log(array.some(even));
-console.log(array.mySome(even2));
-// expected output: true
-*/
+    console.log(array.some(even));
+    console.log(array.mySome(even2));
+    // expected output: true
+    */
 
 
 // EVERY //
@@ -106,15 +106,15 @@ Array.prototype.myEvery = function(callbackFn) {
     return true;
 }; 
 
-/* TEST EVERY
-const isBelowThreshold = (currentValue) => currentValue < 40;
+    /* TEST EVERY
+    const isBelowThreshold = (currentValue) => currentValue < 40;
 
-const array1 = [1, 30, 39, 29, 10, 13];
-const array2 = [1, 30, 39, 29, 10, 13];
-console.log(array1.every(isBelowThreshold));
-console.log(array2.myEvery(isBelowThreshold));
-// expected output: true
-*/
+    const array1 = [1, 30, 39, 29, 10, 13];
+    const array2 = [1, 30, 39, 29, 10, 13];
+    console.log(array1.every(isBelowThreshold));
+    console.log(array2.myEvery(isBelowThreshold));
+    // expected output: true
+    */
 
 // REDUCE //
 Array.prototype.myReduce = function(callbackFn, Values) {
@@ -132,23 +132,24 @@ Array.prototype.myReduce = function(callbackFn, Values) {
     return Values;
 };
 
-/* TEST REDUCE 
-const array1 = [1, 2, 3, 4];
-const reducer = (previousValue, currentValue) => previousValue + currentValue;
-const array2 = [1, 2, 3, 4];
-// 1 + 2 + 3 + 4
-console.log(array1.reduce(reducer));
-console.log(array2.myReduce(reducer));
-// expected output: 10
+    /* TEST REDUCE 
+    const array1 = [1, 2, 3, 4];
+    const reducer = (previousValue, currentValue) => previousValue + currentValue;
+    const array2 = [1, 2, 3, 4];
+    // 1 + 2 + 3 + 4
+    console.log(array1.reduce(reducer));
+    console.log(array2.myReduce(reducer));
+    // expected output: 10
 
-// 5 + 1 + 2 + 3 + 4
-console.log(array1.reduce(reducer, 5));
-console.log(array2.myReduce(reducer, 5));
-// expected output: 15
-*/
-
+    // 5 + 1 + 2 + 3 + 4
+    console.log(array1.reduce(reducer, 5));
+    console.log(array2.myReduce(reducer, 5));
+    // expected output: 15
+    */
 //RC ENDS HERE//
 
+
+//MG STARTS HERE//
 // INCLUDES //
 Array.prototype.myIncludes = function(obj) {
     for (let i = 0; i < this.length; i++) {
@@ -158,22 +159,22 @@ Array.prototype.myIncludes = function(obj) {
     }
     return false;
 };
-    //TEST INCLUDES FUNCTION:
-    const array1 = [1, 2, 3];
+    // //TEST INCLUDES FUNCTION:
+    // const array1 = [1, 2, 3];
 
-    console.log(array1.myIncludes(2));
-    console.log(array1.includes(2));
-    // expected output: true
+    // console.log(array1.myIncludes(2));
+    // console.log(array1.includes(2));
+    // // expected output: true
 
-    const pets = ['cat', 'dog', 'bat'];
+    // const pets = ['cat', 'dog', 'bat'];
 
-    console.log(pets.myIncludes('cat'));
-    console.log(pets.includes('cat'));
-    // expected output: true
+    // console.log(pets.myIncludes('cat'));
+    // console.log(pets.includes('cat'));
+    // // expected output: true
 
-    console.log(pets.myIncludes('at'));
-    console.log(pets.includes('at'));
-    // expected output: false
+    // console.log(pets.myIncludes('at'));
+    // console.log(pets.includes('at'));
+    // // expected output: false
 
 
 // INDEXOF //
@@ -185,19 +186,19 @@ Array.prototype.myIndexOf = function(obj) {
     }
     return -1;
 };
-    //TEST INDEXOF FUNCTION:
-    const beasts = ['ant', 'bison', 'camel', 'duck', 'bison'];
-    console.log(beasts.myIndexOf('bison'));
-    console.log(beasts.indexOf('bison'));
-    // expected output: 1
+    // //TEST INDEXOF FUNCTION:
+    // const beasts = ['ant', 'bison', 'camel', 'duck', 'bison'];
+    // console.log(beasts.myIndexOf('bison'));
+    // console.log(beasts.indexOf('bison'));
+    // // expected output: 1
 
-    console.log(beasts.myIndexOf('ant'));
-    console.log(beasts.indexOf('ant'));
-    // expected output: 0
+    // console.log(beasts.myIndexOf('ant'));
+    // console.log(beasts.indexOf('ant'));
+    // // expected output: 0
 
-    console.log(beasts.myIndexOf('giraffe'));
-    console.log(beasts.indexOf('giraffe'));
-    // expected output: -1
+    // console.log(beasts.myIndexOf('giraffe'));
+    // console.log(beasts.indexOf('giraffe'));
+    // // expected output: -1
 
 // PUSH //
 Array.prototype.myPush = function(...args) {
@@ -210,11 +211,11 @@ Array.prototype.myPush = function(...args) {
     }
     return this.length;
 };
-    //TEST PUSH:
-    const arr = [1, 7];
-    console.log(arr.myPush(10, 20));
-    arr.push(100,200);
-    console.log(arr); //should print [1,7,10,20,100,200]
+    // //TEST PUSH:
+    // const arr = [1, 7];
+    // console.log(arr.myPush(10, 20));
+    // arr.push(100,200);
+    // console.log(arr); //should print [1,7,10,20,100,200]
 
 // LASTINDEXOF //
 Array.prototype.myLastIndexOf = function(obj) {
@@ -225,14 +226,14 @@ Array.prototype.myLastIndexOf = function(obj) {
     }
     return -1;
 };
-    //TEST LASTINDEX FUNCTION:
-    const animals = ['Dodo', 'Tiger', 'Penguin', 'Dodo'];
+    // //TEST LASTINDEX FUNCTION:
+    // const animals = ['Dodo', 'Tiger', 'Penguin', 'Dodo'];
 
-    console.log(animals.myLastIndexOf('Dodo'));
-    // expected output: 3
+    // console.log(animals.myLastIndexOf('Dodo'));
+    // // expected output: 3
 
-    console.log(animals.lastIndexOf('Tiger'));
-    // expected output: 1
+    // console.log(animals.lastIndexOf('Tiger'));
+    // // expected output: 1
 
 
 // KEYS //
@@ -243,15 +244,15 @@ Object.grabKeys = function(obj) {
     }
     return keys;
 };
-    //TEST GRAB KEYS FUNCTION:
-        const object1 = {
-            a: 'somestring',
-            b: 42,
-            c: false
-        };
-    console.log(Object.grabKeys(object1));
-    console.log(Object.keys(object1));
-    // expected output: Array ["a", "b", "c"]
+    // //TEST GRAB KEYS FUNCTION:
+    //     const object1 = {
+    //         a: 'somestring',
+    //         b: 42,
+    //         c: false
+    //     };
+    // console.log(Object.grabKeys(object1));
+    // console.log(Object.keys(object1));
+    // // expected output: Array ["a", "b", "c"]
 
 // VALUES //
 Object.grabValues = function(obj) {
@@ -264,14 +265,13 @@ Object.grabValues = function(obj) {
     return vals;
 };
 
-//Test grabValues
-    const object = {
-        a: 'somestring',
-        b: 42,
-        c: false
-    };
+    // //Test grabValues
+    // const object = {
+    //     a: 'somestring',
+    //     b: 42,
+    //     c: false
+    // };
     
-    console.log(Object.grabValues(object));
-    console.log(Object.values(object));
-
-// expected output: Array ["somestring", 42, false]
+    // console.log(Object.grabValues(object));
+    // console.log(Object.values(object));
+    // // expected output: Array ["somestring", 42, false]
